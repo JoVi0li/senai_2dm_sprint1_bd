1884af152e1e06b4bd52ea96b233e94c5da4c341
João Vitor <Jovi.oli04@gmail.com>
2021-03-02 08:35:36 -0300

    MER finalizado

---
+++ HROADS/Scripts/hroads_01_DDL.sql
@@ -0,0 +1,36 @@
+CREATE DATABASE SENAI_HROADS_MANHA; 
+
+USE SENAI_HROADS_MANHA;
+
+CREATE TABLE TipoHabilidade
+(
+	idTipoHabilidade INT PRIMARY KEY IDENTITY
+	,NomeTipoHabilidade VARCHAR(10) NOT NULL 
+);
+
+CREATE TABLE Habilidades
+(
+	idHabilidade INT PRIMARY KEY IDENTITY
+	,idTipoHabilidade INT FOREIGN KEY REFERENCES TipoHabilidade(idTipoHabilidade)
+	,NomeHabilidade VARCHAR(200) NOT NULL 
+);
+
+CREATE TABLE Classes
+(
+	idClasse INT PRIMARY KEY IDENTITY
+	,idHabilidade INT FOREIGN KEY REFERENCES Habilidades(idHabilidade)
+	,NomeClasse VARCHAR(200) NOT NULL 
+);
+
+CREATE TABLE HabilidadeClasses
+(
+	idClasse INT FOREIGN KEY REFERENCES Classes(idClasse)
+	,idHabilidade INT FOREIGN KEY REFERENCES Habilidades(idHabilidade)
+);
+
+CREATE TABLE Personagens
+(
+	idPersonagem INT PRIMARY KEY IDENTITY
+	,idClasse INT FOREIGN KEY REFERENCES Classes(idClasse)
+	,NomePersonagem VARCHAR(200) NOT NULL
+);
